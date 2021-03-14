d28d7562f39b38d7e87103b6a40bbf818a195bb2
Jirachai Thiemsert <cray.j.thiemsert@gmail.com>
2021-03-14 14:09:10 +0700

    "the Node": - Load and create new config file.

--- ftiotsystem/lib/main.dart
+++ ftiotsystem/lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Demox',
       theme: ThemeData(
         // This is the theme of your application.
         //
